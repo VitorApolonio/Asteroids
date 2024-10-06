66c46a8e59dc3803a921e1b8daa3dc73e4a5b033
VitorApolonio <vitorapolonio74@gmail.com>
2024-10-06 12:25:11 -0300

    Improve documentation of Asteroid class

--- src/main/java/dev/apolonio/asteroids/domain/Asteroid.java
+++ src/main/java/dev/apolonio/asteroids/domain/Asteroid.java
@@ -4,26 +4,44 @@ import dev.apolonio.asteroids.PolygonFactory;
 
 import java.util.Random;
 
+/**
+ * Represents an asteroid in the game.
+ * <p>
+ * Asteroids move around randomly in the screen, slowly rotating. They can collide with the player ship but not with
+ * one another.
+ */
 public class Asteroid extends Character {
     
     private double rotationalMovement;
-    
+
+    /**
+     * Creates a new asteroid at position X, Y.
+     *
+     * @param x the X coordinate for the asteroid position
+     * @param y the Y coordinate for the asteroid position
+     */
     public Asteroid(int x, int y) {
+        // Creates a new Character using a polygon from PolygonFactory
         super(new PolygonFactory().createPolygon(), x, y);
-        
+
+        // Randomize inicial rotation
         Random rand = new Random();
-        
         super.getCharacter().setRotate(rand.nextInt(360));
-        
+
+        // Randomize acceleration
         int acceleration = 4 + rand.nextInt(10);
         
         for (int i = 0; i < acceleration; i++) {
             accelerate();
         }
-        
+
+        // Randomize rotational velocity
         rotationalMovement = 0.5 - rand.nextDouble();
     }
-    
+
+    /**
+     * Moves the asteroid in the direction it's facing and rotates it.
+     */
     @Override
     public void move() {
         super.move();
